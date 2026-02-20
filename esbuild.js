efef202698e42967b0220ae2d6437bf6621405f6
Adrian Latorre <adrian.latorre@revvity.com>
2026-02-20 16:14:30 +0100

    feat: add release scripts

---
+++ esbuild.js
@@ -0,0 +1,56 @@
+const esbuild = require('esbuild');
+
+const production = process.argv.includes('--production');
+const watch = process.argv.includes('--watch');
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+  name: 'esbuild-problem-matcher',
+  setup(build) {
+    build.onStart(() => {
+      console.log('[watch] build started');
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        if (location == null) {
+          return;
+        }
+        console.error(`    ${location.file}:${location.line}:${location.column}:`);
+      });
+      console.log('[watch] build finished');
+    });
+  }
+};
+
+async function main() {
+  const ctx = await esbuild.context({
+    entryPoints: ['src/extension.ts'],
+    bundle: true,
+    format: 'cjs',
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: 'node',
+    target: 'node20',
+    outfile: 'out/extension.js',
+    external: ['vscode'],
+    logLevel: 'warning',
+    plugins: [esbuildProblemMatcherPlugin]
+  });
+
+  if (watch) {
+    await ctx.watch();
+    return;
+  }
+
+  await ctx.rebuild();
+  await ctx.dispose();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
